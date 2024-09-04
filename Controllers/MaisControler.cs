e818f309b1c7132435374cf0594679a6b888fbe2
JuliaPaulaMenegazzoSesi <159256141+JuliaPaulaMenegazzoSesi@users.noreply.github.com>
2024-09-04 10:54:02 -0300

    Add files via upload

---
+++ Controllers/MaisControler.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace YourProjectNamespace.Controllers
+{
+    public class MaisController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Port()
+        {
+            return View();
+        }
+    }
+}
